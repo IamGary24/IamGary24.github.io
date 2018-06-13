7bc222392c37b800489c7f094ace1f23e8e98692
IamGary24 <gfowl0813@gmail.com>
2018-06-13 13:04:32 -0500

    More work on drawing spirals

--- drawSwirls.js
+++ drawSwirls.js
@@ -4,11 +4,19 @@ function drawYellow() {
 	var canvas = document.getElementById("swirl_yellow");
 	var context = canvas.getContext("2d");
 	var color = "#FFFF00";
+	var theta = 2*Math.PI/60;
+	var increment = theta;
 	
 	context.strokeStyle=color;
-	context.beginPath();
-	context.arc(95,50,40,0,2*Math.PI);
+	
+	while(theta < 10*Math.PI) {
+		x = 50 + theta * Math.cos(theta);
+		y = 150 + theta * Math.sin(theta);
+		context.lineTo(x, y);
+		theta += increment;
+	}
 	context.stroke();
+
 	}
 	
 drawYellow();
